4e094fbfe6610a48eafd5654f34b0e0abcdb3a81
Ramesh Adapa <rameshadapa32@gmail.com>
2015-02-06 16:48:27 +0530

    [Day14] Simplified alpha masking.

--- Resources/game.glsl
+++ Resources/game.glsl
@@ -4,25 +4,14 @@ varying vec2 v_texCoord;
 
 uniform vec2 center;
 uniform vec2 resolution;
-//uniform float lightcount;
-uniform vec2 lightposition;	//[5];
+uniform vec2 lightposition;
 //CC_Time[1] is time
 
 void main()			
 {
 	vec4 c = v_fragmentColor * texture2D(CC_Texture0, v_texCoord);
 	float d = resolution.x/8.0;
-	float opacity = 1.0;
-	int i;
-	for(i=0; i<1; ++i)
-	{
-		float dist = distance(gl_FragCoord.xy, lightposition);
-		if(dist < 1.0)
-		    dist = dist*d;
-		else
-		    dist = (dist/d);
-		opacity = dist;
-	}
+	float dist = distance(gl_FragCoord.xy, lightposition);
 	gl_FragColor.rgb = c.rgb;
-	gl_FragColor.a = opacity;
+	gl_FragColor.a = dist/d;
 }				
